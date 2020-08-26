b6bbbd2e650ebf0d95c0b59e86be819e2d1d9ca1
Yoshiaki Ito <ito.yoshiaki@canon-its.co.jp>
2020-08-26 14:17:04 +0900

    2nd commit

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const db = require('./db');
 
 app.get('/', function(req, res) {
   res.send('OK');
+});
+
+app.get('/create', function(req, res) {
+  db.pool.connect((err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query("CREATE TABLE Rank(id SERIAL,hands INTEGER NOT NULL,name VARCHAR(10) NOT NULL,level INTEGER NOT NULL,PRIMARY KEY(id))", function(err, result) {
+        console.log(result);
+      });
+    }
+  });
+  res.render('index', {
+    title: 'create',
+  });
+});
+
+app.get('/add', function(req, res) {
+  db.pool.connect((err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query("INSERT INTO Rank (hands, name, level) VALUES (10, 'taro', 2)", function(err, result) {
+        console.log(result.rows);
+      });
+    }
+  });
+  res.render('index', {
+    title: 'add',
+  });
+});
+
+app.get('/get', function(req, res) {
+  db.pool.connect((err, client) => {
+    if (err) {
+      console.log(err);
+    } else {
+      client.query("SELECT name, hands FROM rank", function(err, result) {
+        console.log(result.rows);
+      });
+    }
+  });
+  res.render('index', {
+    title: 'get',
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
