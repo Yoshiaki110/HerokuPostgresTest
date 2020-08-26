07ffb7bdfc3a06d7247bc309212f3d27b94724cf
Yoshiaki Ito <ito.yoshiaki@canon-its.co.jp>
2020-08-26 14:24:32 +0900

    3rd Commit

--- index.js
+++ index.js
@@ -9,46 +9,43 @@ app.get('/', function(req, res) {
 app.get('/create', function(req, res) {
   db.pool.connect((err, client) => {
     if (err) {
-      console.log(err);
+      console.log('connect error', err);
     } else {
       client.query("CREATE TABLE Rank(id SERIAL,hands INTEGER NOT NULL,name VARCHAR(10) NOT NULL,level INTEGER NOT NULL,PRIMARY KEY(id))", function(err, result) {
-        console.log(result);
+        console.log('err', err);
+        console.log('result', result);
       });
     }
   });
-  res.render('index', {
-    title: 'create',
-  });
+  res.send('OK');
 });
 
 app.get('/add', function(req, res) {
   db.pool.connect((err, client) => {
     if (err) {
-      console.log(err);
+      console.log('connect error', err);
     } else {
       client.query("INSERT INTO Rank (hands, name, level) VALUES (10, 'taro', 2)", function(err, result) {
-        console.log(result.rows);
+        console.log('err', err);
+        console.log('result.rows', result.rows);
       });
     }
   });
-  res.render('index', {
-    title: 'add',
-  });
+  res.send('OK');
 });
 
 app.get('/get', function(req, res) {
   db.pool.connect((err, client) => {
     if (err) {
-      console.log(err);
+      console.log('connect error', err);
     } else {
       client.query("SELECT name, hands FROM rank", function(err, result) {
-        console.log(result.rows);
+        console.log('err', err);
+        console.log('result.rows', result.rows);
       });
     }
   });
-  res.render('index', {
-    title: 'get',
-  });
+  res.send('OK');
 });
 
 var port = Number(process.env.PORT || 5000);
